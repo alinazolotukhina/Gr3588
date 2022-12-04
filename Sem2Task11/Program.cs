3e7175e134ecff9032e66c1d70d19bf9a03596c6
laim31@gmail.com <laim31@mail.ru>
2022-12-04 23:23:27 +0300

    add solution for task 9,11,12,14,16

---
+++ Sem2Task11/Program.cs
@@ -0,0 +1,14 @@
+﻿//Создаем экземпляр класса рандомайзер
+System.Random numberSintezator = new System.Random();
+
+//Получаем новое случайное число
+int number = numberSintezator.Next(100,1000);
+//Выводим данные в консоль
+Console.WriteLine (number);
+//Получаем старший разряд числа
+int firstNumber = number/100;
+//Получаем младший разряд числа
+int secondNumber = number%10;
+
+int result = firstNumber*10 + secondNumber;
+Console.WriteLine (result);
---
+++ Sem2Task16/Program.cs
@@ -0,0 +1,14 @@
+﻿void TestSQRT(int i, int j)
+{
+    bool result = (i / j == j);
+    if (result == true) Console.WriteLine(" Число " + i + " является квадратом числа " + j);
+    else Console.WriteLine(" Число " + i + " не является квадратом числа " + j);
+}
+
+int number1 = int.Parse(Console.ReadLine() ?? "0");
+//Console.WriteLine(number1);
+int number2 = int.Parse(Console.ReadLine() ?? "0");
+//Console.WriteLine(number2);
+
+TestSQRT(number1, number2);
+TestSQRT(number2, number1);

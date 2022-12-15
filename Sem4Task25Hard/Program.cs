706b2064f8a1da41f2787a44875ba241b133df09
laim31@gmail.com <laim31@mail.ru>
2022-12-15 22:51:41 +0300

    add solution for task 25Hard,27,29

---
+++ Sem4Task25Hard/Program.cs
@@ -0,0 +1,70 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+try
+{
+    double number1 = ReadData("Введите первое число: ");
+    string oper = ReadStringData("Введите операцию: ");
+    double number2 = ReadData("Введите второе число ");
+    double result = Calculation(number1, oper, number2);
+    PrintData(number1 + " " + oper + " " + number2 + " = " + result);
+}
+catch (Exception ex)
+{
+    PrintData(ex.Message);
+}
+
+double ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return double.Parse(Console.ReadLine() ?? "0");
+}
+
+string ReadStringData(string msg)
+{
+    Console.WriteLine(msg);
+    return Console.ReadLine() ?? "0";
+}
+
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+double Pow(double num, double pow)
+{
+    return Math.Pow(num, pow);
+}
+double Sum(double num1, double num2)
+{
+    return num1 + num2;
+}
+double Diff(double num1, double num2)
+{
+    return num1 - num2;
+}
+
+double Mult(double num1, double num2)
+{
+    return num1 * num2;
+}
+
+double Div(double num1, double num2)
+{
+    if (num2 == 0)
+        throw new Exception("Деление на ноль невозможно");
+    else return num1 / num2;
+}
+
+double Calculation(double number1, string oper, double number2)
+{
+    switch (oper)
+    {
+        case "+": return Sum(number1, number2);
+        case "-": return Diff(number1, number2);
+        case "*": return Mult(number1, number2);
+        case "/": return Div(number1, number2);
+        case "^": return Pow(number1, number2);
+        default:
+            throw new System.Exception("Недопустимая операция");
+    }
+}
---
+++ Sem4Task29/Program.cs
@@ -0,0 +1,19 @@
+﻿string ReadStringData(string msg)
+{
+    Console.WriteLine(msg);
+    return Console.ReadLine() ?? "0";
+}
+
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+string NameChoice(string str)
+{
+    string[] names = str.Split(",");
+    string name = names[new Random().Next(0, names.Length)].Trim();
+    return name;
+}
+string str = ReadStringData("Введите имена через запятую: ");
+PrintData(""+ NameChoice(str));
